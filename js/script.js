4830a8eba1e14ec503193c081904ac400c18ee01
vmuiru <terrmoire@me.com>
2020-10-17 16:14:19 -0400

    form function test

--- js/script.js
+++ js/script.js
@@ -1,21 +1,49 @@
 // Constants and Variables
 const API_KEY = '89194dedd2cd4815893a2c41dbd7adcb'; //CONFIG.newsAPIKey
-const BASE_URL = 'https://newsapi.org/v2/top-headlines?';
+const BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apikey=';
 
-//Cached element references
+//cached element references
+const $title = $('#title');
+const $author = $('#author');
+const $image = $('#image');
+const $form = $('form');
+const $input = $('input[type="text"]');
 
+let userInput; 
 
-
-// event listeners
-
-
+// event listeners 
+$form.on('submit', handleGetData);
 
 // functions
+function handleGetData(event) {
+    event.preventDefault();
 
-$.ajax(BASE_URL + API_KEY)
+     userInput = $input.val();
+
+    $.ajax(BASE_URL +`q=${userInput}&` + `units=&appid=`+ API_KEY)
     .then(function(data) {
-        newsData = data;
-        render();
+        console.log('DATA: ', data);
+
+        render(data);
     }, function(error) {
         console.log('Error: ', error);
     });
+}
+
+
+function render(data) {
+    $title.text('Title: ' + data.title);
+    $author.text('Author: ' + data.author);
+    // $feelsLike.text('Feels Like: ' + data.main.feels_like);
+    // $currentWeather.text('Current Weather: ' + data.weather[0].description);
+};
+
+// $currentWeather.grep(data.weather[0].description, funcition('cloud')) {
+// render('<img src="/.img/yang-yang-HqprR1VEDjw-unsplash.jpg">'); 
+// };
+
+
+// if($currentWeather.text === 'scattered clouds') {
+//     render('<img src="/.img/yang-yang-HqprR1VEDjw-unsplash.jpg">');
+// }
+
