46869dc1269cdddadc356358a668fb075c74a880
vmuiru <terrmoire@me.com>
2020-10-17 13:16:49 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Constants and Variables
+const API_KEY = '89194dedd2cd4815893a2c41dbd7adcb'; //CONFIG.newsAPIKey
+const BASE_URL = 'https://newsapi.org/v2/top-headlines?';
+
+//Cached element references
+
+
+
+// event listeners
+
+
+
+// functions
