c45234fd51937e88c9158aa5b45394dd93897983
vmuiru <terrmoire@me.com>
2020-10-20 22:02:08 -0400

    second draft beautify implemented

--- js/script.js
+++ js/script.js
@@ -17,64 +17,30 @@ $form.on('submit', handleGetHeadlines);
 function handleGetHeadlines(event) {
     event.preventDefault();
 
-     userInput = $input.val();
+    userInput = $input.val();
 
-    $.ajax(BASE_URL + '?category=' +userInput)
-    .then(function(headlines) {
-        publicationData = headlines.sources;
-        render();
+    $.ajax(BASE_URL + '?category=' + userInput)
+        .then(function (headlines) {
+            publicationData = headlines.sources;
+            render();
 
-        render(headlines);
-    }, function(error) {
-        console.log('Error: ', error);
-    });
+            render(headlines);
+        }, function (error) {
+            console.log('Error: ', error);
+        });
 }
 
 
 
 function render() {
-    const html = publicationData.map(function(article) {
-        return`
+    const html = publicationData.map(function (article) {
+        return `
             <h2>${article.name}</h2>
             <p>${article.description}</p>
         `
     })
-    //${article.url}">Link to Article</a>
 
-    //var result = str.link(article.url);
-    // document.querySelector(article.description) {
-    //     string.link(`<a href="${article.url}">Link to Article</a>`)};
     $('#articles-container').append(html);
-    
-    
+
+
 };
-
-
-// Document.querySelector('p') {
-
-
-// }
-// document.createEvent('MouseEvent');
-
-
-
-
-// function render(fullStory) {
-//     $publicationData.article.url({
-//         src: publicationData.article.url,
-//         alt: publicationData.article.name
-//     });
-// }
-
-
-
-/*
-visit eye stories.com
-home page - top headlines images in us
-12 photos from
-  - hover to see article title and author
-  -click to read full story
-
-  search by author, source maybe date
-
-*/
